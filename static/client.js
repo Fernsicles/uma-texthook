43f1f4f6581b6dd3abc8fbce00d7e97f2acf17ed
Ferns <jeffreyzhang123@gmail.com>
2023-11-05 13:52:42 -0800

    Log parsed json

--- static/client.js
+++ static/client.js
@@ -1,19 +1,19 @@
 let socket = io();
 socket.on('message', (msg) => {
-    console.log(msg);
-    let sceneList = document.getElementById('sceneList')
+    let sceneList = document.getElementById('sceneList');
     let scenes = JSON.parse(msg);
+    console.log(scenes);
     scenes.forEach((i) => {
         let scene = Object.assign(document.createElement('li'));
-        let title = Object.assign(document.createElement('div'), {textContent: i.Title});
+        let title = Object.assign(document.createElement('div'), { textContent: i.Title });
         scene.append(title);
         let dialog = Object.assign(document.createElement('ul'));
         scene.append(dialog);
         i.Dialog.forEach((i) => {
             let element = Object.assign(document.createElement('li'));
-            let name = Object.assign(document.createElement('div'), {textContent: i.Name});
+            let name = Object.assign(document.createElement('div'), { textContent: i.Name });
             element.append(name);
-            let text = Object.assign(document.createElement('div'), {textContent: i.Text});
+            let text = Object.assign(document.createElement('div'), { textContent: i.Text });
             element.append(text);
             dialog.append(element);
         });
